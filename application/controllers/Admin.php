3c85f780d725724a85b58dcc8f4efedd0018f06b
darkPigeonn <50091059+darkPigeonn@users.noreply.github.com>
2022-01-04 15:05:56 +0700

    template untuk function crud

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,4 +9,45 @@ class Admin extends CI_Controller {
 		$this->load->view('admin/home');
 		$this->load->view('templates/footer_admin');
 	}
+	//CRUD for INPUT QUESTION
+	public function inputItem()
+	{
+	}
+	public function editItem()
+	{
+	}
+	public function getItem()
+	{
+	}
+	public function delItem()
+	{
+	}
+	
+	//CRUD for Group Question
+	public function inputGroup()
+	{
+	}
+	public function editGroup()
+	{
+	}
+	public function getGroup()
+	{
+	}
+	public function delGroup()
+	{
+	}
+	
+	//CRUD for Section Question
+	public function inputSection()
+	{
+	}
+	public function editSection()
+	{
+	}
+	public function getSection()
+	{
+	}
+	public function delGroup()
+	{
+	}
 }
