c5b75bcaae7993b863d46e4e8b4401059a56f5a2
darkPigeonn <50091059+darkPigeonn@users.noreply.github.com>
2022-01-04 15:24:15 +0700

    guide untuk isi function

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,16 +2,63 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('admin_model');
+		$this->load->helper('url');
+ 
+	}
+	//load view
 	public function index()
 	{
 		$this->load->view('templates/header_admin');
 		$this->load->view('admin/home');
 		$this->load->view('templates/footer_admin');
 	}
+	public function formItemInput()
+	{
+		$this->load->view('templates/header_admin');
+		$this->load->view('admin/form/itemInput');
+		$this->load->view('templates/footer_admin');
+	}
+	
+	public function formGroupInput()
+	{
+		$this->load->view('templates/header_admin');
+		$this->load->view('admin/form/groupInput');
+		$this->load->view('templates/footer_admin');
+	}
+	
+	public function formSectionInput()
+	{
+		$this->load->view('templates/header_admin');
+		$this->load->view('admin/form/sectionInput');
+		$this->load->view('templates/footer_admin');
+	}
 	//CRUD for INPUT QUESTION
 	public function inputItem()
 	{
+		//pertanyaan
+		$question = $this->input->post('question');
+		//jawaban
+		$answer1 = $this->input->post('answer1');
+		$answer2 = $this->input->post('answer2');
+		$answer3 = $this->input->post('answer3');
+		$answer4 = $this->input->post('answer4');
+		
+		$dataAnswers = array(
+			'answer1' => $answer1,
+			'answer2' => $answer2,
+			'answer3' => $answer3,
+			'answer4' => $answer4
+		);
+		
+		$dataItem = array (
+			'question' => $qeustion,
+			'answers' => $dataAnswers
+			);
+		$this->admin_model->inputItem($dataItem);
+		
 	}
 	public function editItem()
 	{
